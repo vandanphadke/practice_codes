ff2f95ca5d5ef6f4f820e699d5196b93f74e3f39
vandan.p <vandan.p@endurance.com>
2018-05-18 17:13:59 +0530

    Corrected optimal matrix multiplication DP

--- DynamicProgramming/src/Basic/MatrixMultiply.java
+++ DynamicProgramming/src/Basic/MatrixMultiply.java
@@ -1,26 +1,42 @@
 package Basic;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class MatrixMultiply {
 	public static void main(String[] args) {
-		
+		int[] cost = new int[]{30, 35, 15, 5, 10, 20, 25};
+		System.out.println(optimalmatrixMultiply(cost));
 	}
 	
-	public static void optimalmatrixMultiply(int[] mat){
-		int[][] data = new int[mat.length][mat.length];
-		for(int i = 0 ; i < data.length ; i++)
+	public static int optimalmatrixMultiply(int[] mat){
+		int n = mat.length - 1;
+		int[][] data = new int[n + 1][n + 1];
+
+		// Cost of multiplying a matrix to itself is 0
+		for(int i = 1 ; i < n+1 ; i++)
 			data[i][i] = 0 ;
-		int j , q; 
-		for(int l = 2 ; l < data.length ; l++){
-			for(int i = 1 ; i <= data.length - l + 1 ; i++){
+
+		int j , q;
+
+		for(int l = 2 ; l <= n ; l++) {
+			for(int i = 1 ; i <= n - l + 1 ; i++){
 				j = i + l - 1 ;
 				data[i][j] = Integer.MAX_VALUE;
-				for(int k = i ; k <= j - 1 ; k++){
+				System.out.println("Current i: " + i + " Current j: " + j);
+				for(int k = i ; k < j ; k++){
 					q = data[i][k] + data[k+1][j] + mat[i-1]*mat[k]*mat[j];
 					if(q < data[i][j])
 							data[i][j] = q ; 
 				}
 			}
 		}
-		System.out.println(data[1][mat.length - 1]);
+
+
+		for (int i = 0; i < data.length; i++) {
+			System.out.println(Arrays.toString(data[i]));
+		}
+
+		return data[1][n];
 	}
 }
